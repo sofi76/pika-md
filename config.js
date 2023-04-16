9d3008eca3cdb4f6228baafee2d1d2208854b021
sofi76 <127637391+sofi76@users.noreply.github.com>
2023-04-16 12:57:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6285693048688'],
+  ['6285693048688'],
+  ['6285693048688', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
